4722ffe432464e97311dc532c2397d0f1a053543
Pouya Ataei <pouya-ataei@idexx.com>
2024-01-15 21:47:09 +1300

    working on the actual selection of the emails

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto("https://developer.chrome.com/");
-
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  // Type into search box
-  await page.type(".search-box__input", "automate beyond recorder");
-
-  // Wait and click on first result
-  const searchResultSelector = ".search-box__link";
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    "text/Customize and automate"
+  // Navigate to the main page
+  await page.goto(
+    "https://www.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/academic-staff"
   );
-  const fullTitle = await textSelector?.evaluate((el) => el.textContent);
 
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  // Extract the URLs of each staff member
+  const staffUrls = await page.$$eval(
+    "#content_container_257037 p a",
+    (links) => links.map((link) => link.href)
+  );
+
+  // Loop through each URL
+  for (const url of staffUrls) {
+    await page.goto(url);
+
+    // Replace 'selector-for-email' with the actual selector for the email element
+    const email = await page.$eval(
+      'ul[role="list"] li[role="listitem"] a[href^="mailto:"]',
+      (element) => element.textContent.trim()
+    );
+
+    console.log(`Email: ${email}`);
+  }
 
   await browser.close();
 })();

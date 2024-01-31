32569d348969f5bef36d130e8fe4000c34affc41
Pouya Ataei <pouya-ataei@idexx.com>
2024-01-31 16:15:38 +1300

    updated the stuff

--- melbourne.ts
+++ melbourne.ts
@@ -5,10 +5,11 @@ import { insertEmail } from "./insertEmailIntoDB";
 
 (async () => {
   let browser;
-  // Initialise the database
-  const db = startupTheService();
 
   try {
+    // Initialise the database
+    const db = startupTheService();
+
     // Start Puppeteer
     browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
@@ -16,39 +17,46 @@ import { insertEmail } from "./insertEmailIntoDB";
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     );
 
-    // Define the URL to scrape
-    const academicStaffsPage =
-      "https://sgeas.unimelb.edu.au/about/people/academic-staff";
+    // Define the URLs to scrape
+    const academicStaffPages = [
+      "https://sgeas.unimelb.edu.au/about/people/academic-staff",
+      "https://cis.unimelb.edu.au/people#academic",
+    ];
 
-    try {
-      const response = await page.goto(academicStaffsPage, {
-        waitUntil: "networkidle0",
-        timeout: 30000,
-      });
+    for (const academicStaffPage of academicStaffPages) {
+      try {
+        const response = await page.goto(academicStaffPage, {
+          waitUntil: "networkidle0",
+          timeout: 30000,
+        });
 
-      console.log(response.status());
-
-      if (response && response.ok()) {
-        // Wait for the page to load completely
-        const emailSelector = 'td[data-label="Email"] a[href^="mailto:"]';
-
-        const emailsElements = await page.$$eval(emailSelector, (elements) =>
-          elements.map((element) => element.textContent.trim())
+        console.log(
+          `Visiting: ${academicStaffPage} - Status: ${response.status()}`
         );
 
-        for (const email of emailsElements) {
-          try {
-            await insertEmail(db, email);
-          } catch (error) {
-            console.error(`Error inserting email ${email}: `, error);
+        if (response && response.ok()) {
+          // Wait for the page to load completely
+          const emailSelector =
+            'td[data-label="Email"] a[href^="mailto:"], a[href^="mailto:"]'; // Adjusted to include both formats
+
+          const emailsElements = await page.$$eval(emailSelector, (elements) =>
+            elements.map((element) => element.textContent.trim())
+          );
+
+          for (const email of emailsElements) {
+            try {
+              await insertEmail(db, email);
+            } catch (error) {
+              console.error(`Error inserting email ${email}: `, error);
+            }
           }
         }
+      } catch (error) {
+        console.error(
+          `Error on academic staff page ${academicStaffPage}: `,
+          error
+        );
       }
-    } catch (error) {
-      console.error(
-        `Error on academic staff page ${academicStaffsPage}: `,
-        error
-      );
     }
   } catch (error) {
     console.error("An error occurred:", error);
@@ -58,6 +66,6 @@ import { insertEmail } from "./insertEmailIntoDB";
     }
 
     // Clean up the database connection
-    cleanUp(db, browser);
+    cleanUp(db);
   }
 })();
